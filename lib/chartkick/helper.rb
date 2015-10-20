748add6dd225638384247436ab6c2daf6ea6fe2b
Peter Backman <peter@iostream.cc>
2015-10-20 14:47:28 +0200

    Option for HTML class attribute

--- lib/chartkick/helper.rb
+++ lib/chartkick/helper.rb
@@ -42,10 +42,12 @@ module Chartkick
       options = chartkick_deep_merge(Chartkick.options, options)
       element_id = options.delete(:id) || "chart-#{@chartkick_chart_id += 1}"
       height = options.delete(:height) || "300px"
+      className = options.delete(:class) || ""
+
       # content_for: nil must override default
       content_for = options.key?(:content_for) ? options.delete(:content_for) : Chartkick.content_for
 
-      html = (options.delete(:html) || %(<div id="%{id}" style="height: %{height}; text-align: center; color: #999; line-height: %{height}; font-size: 14px; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Verdana, Arial, Helvetica, sans-serif;">Loading...</div>)) % {id: ERB::Util.html_escape(element_id), height: ERB::Util.html_escape(height)}
+      html = (options.delete(:html) || %(<div class="%{className}" id="%{id}" style="height: %{height}; text-align: center; color: #999; line-height: %{height}; font-size: 14px; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Verdana, Arial, Helvetica, sans-serif;">Loading...</div>)) % {className: className, id: ERB::Util.html_escape(element_id), height: ERB::Util.html_escape(height)}
 
       js = <<JS
 <script type="text/javascript">
